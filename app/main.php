ad2bbffc93f9147415e2faa4a664bec49d13c42e
Masato Yoshioka <yoshioka0627@gmail.com>
2023-03-30 04:11:28 +0900

    pong

--- app/main.php
+++ app/main.php
@@ -2,14 +2,17 @@
 error_reporting(E_ALL);
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-echo "Logs from your program will appear here";
+//echo "Logs from your program will appear here";
 
 // Uncomment this to pass the first stage
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($sock, SOL_SOCKET, SO_REUSEPORT, 1);
 socket_bind($sock, "localhost", 6379);
 socket_listen($sock, 5);
-socket_accept($sock); // Wait for first client
+$socket = socket_accept($sock);
 
-// socket_close($sock);
+$response = "+PONG\r\n";
+socket_write($socket, $response, strlen($response));
+
+socket_close($sock);
 ?>
