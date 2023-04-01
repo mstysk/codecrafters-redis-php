752d01d62fadc5ea983b4b612622cfcf6e31819a
Masato Yoshioka <yoshioka0627@gmail.com>
2023-04-02 08:59:06 +0900

    multipile PINGs

--- app/main.php
+++ app/main.php
@@ -9,10 +9,47 @@ $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($sock, SOL_SOCKET, SO_REUSEPORT, 1);
 socket_bind($sock, "localhost", 6379);
 socket_listen($sock, 5);
-$socket = socket_accept($sock);
 
-$response = "+PONG\r\n";
-socket_write($socket, $response, strlen($response));
+$clients[] = $sock;
+$write = null;
+$expect = null;
+$second = 0;
 
+while(true) {
+    $read = $clients;
+    $selected  = socket_select($read, $write, $expect, $second);
+    if ($selected === false) {
+        echo 'socket select is failed.';
+        var_dump([$clients, $write, $expect]);
+        break;
+    }
+    if ($selected  <= 0 ){
+        continue;
+    }
+    if(in_array($sock, $read)) {
+        $newSocket = socket_accept($sock);
+        if ($newSocket === false) {
+            echo 'new socket accept failed.';
+            break;
+        }
+        $clients[] = $newSocket;
+        echo "There are " . (count($clients) - 1)." clients";
+        $key = array_search($sock, $read);
+        unset($read[$key]);
+    }
+    foreach($read as $readSock) {
+        $mess = socket_read($readSock, 2048, PHP_NORMAL_READ);
+        if ($mess === false) {
+            $key = array_search($readSock, $clients);
+            unset($clients[$key]);
+            echo "client disconnected.\n";
+            continue;
+        }
+        $mess = trim($mess);
+        if ($mess == "ping") {
+            $response = "+PONG\r\n";
+            socket_write($readSock, $response, strlen($response));
+        }
+    }
+}
 socket_close($sock);
-?>
